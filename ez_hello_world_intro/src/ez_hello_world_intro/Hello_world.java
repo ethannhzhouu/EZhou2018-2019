020aff69dbd5e30d69096de6c50fb80fa760a20e
APCS3rd <APCS3rd@MHS-Math2-31>
2018-08-20 10:43:35 -0700

    First commit to github

---
+++ ez_hello_world_intro/src/ez_hello_world_intro/Hello_world.java
@@ -0,0 +1,15 @@
+package ez_hello_world_intro;
+
+/**
+ * Prints message in console
+ * 
+ * @author Ethan Zhou 
+ * @version 8/17/18
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Ethan Zhou"); //Display the string
+    }
+}
